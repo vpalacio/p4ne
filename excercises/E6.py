d0fdb33710b93d5dba7608fd79c319f7e5ca3ce9
Victor Palacio <vpalacio@ise.com>
2016-04-12 22:46:48 -0400

    Excercise 6 answers

---
+++ excercises/E6.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+'''
+Python for Network Engineers: P4NE - Bring it!
+
+Excercise 6
+'''
+
+import yaml
+import json
+
+__author__ = "Victor Palacio"
+__license__ = "GPL"
+__version__ = "1.0"
+__email__ = "Github - @vpalacio"
+
+
+def main():
+
+    my_dict = {
+      'Course': 'P4NE',
+      'Instructor': 'Kirk Byers'
+    }
+
+    my_list = [
+      'Python for Network Engineers',
+      my_dict
+    ]
+
+    with open("E6.yml", "w") as f:
+        f.write(yaml.dump(my_list, default_flow_style=False))
+
+    with open("E6.json", "w") as f:
+        json.dump(my_list, f)
+
+if __name__ == '__main__':
+    main()
