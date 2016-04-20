4836c152c22ea142cda8f788d183385b7395c8cc
Victor Palacio <vpalacio@ise.com>
2016-04-20 16:53:53 -0400

    tweak

--- excercises/Week 2/E2.py
+++ excercises/Week 2/E2.py
@@ -21,13 +21,19 @@ def main():
 	ip_addr = '50.76.53.27'
 	username = 'pyclass'
 	password = '88newclass'
+	command1 = 'terminal length 0'
+	command2 = 'show ip int brief'
 	remote_conn = telnetlib.Telnet(ip_addr, TELNET_PORT, TELNET_TIMEOUT)
 	output = remote_conn.read_until("sername:", TELNET_TIMEOUT)
 	remote_conn.write( username + '\n')
 	output = remote_conn.read_until("assword:", TELNET_TIMEOUT)
 	remote_conn.write( password + '\n')
 	output = remote_conn.read_very_eager()
+	remote_conn.write( command1 + '\n')
+	remote_conn.write( command2 + '\n')
+	output = remote_conn.read_very_eager()
 	print output
+
 	remote_conn.close()
 
 if __name__ == '__main__':
