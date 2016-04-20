814f40867232bed07b03a9c294b81443905dfca9
Victor Palacio <vpalacio@ise.com>
2016-04-20 16:20:43 -0400

    tweaking

--- excercises/Week 2/E2.py
+++ excercises/Week 2/E2.py
@@ -20,7 +20,8 @@ TELNET_TIMEOUT = 6
 def main():
 
 	remote_conn = telnetlib.Telnet(ip_addr, TELNET_PORT, TELNET_TIMEOUT)
-	remote_conn.read_until("sername", TELNET_TIMEOUT)
+	output = remote_conn.read_until("sername", TELNET_TIMEOUT)
+	print output
 	remote_conn.read_very_eager()
 	print remote_conn
 	# remote_conn.write(<command> + '\n')
