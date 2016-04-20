eb2d3f6ae473bb19e79224c6a1a0f4c6acb555be
Victor Palacio <vpalacio@ise.com>
2016-04-20 16:01:37 -0400

    adding E2

---
+++ excercises/Week 2/E2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+'''
+Python for Network Engineers: P4NE - Bring it!
+
+Excercise 2A
+'''
+import telnetlib
+
+__author__ = "Victor Palacio"
+__license__ = "GPL"
+__version__ = "1.0"
+__email__ = "Github - @vpalacio"
+
+ip_addr = '50.76.53.27'
+TELNET_PORT = 23
+TELNET_TIMEOUT = 6
+
+
+def main():
+
+remote_conn = telnetlib.Telnet(ip_addr, TELNET_PORT, TELNET_TIMEOUT)
+remote_conn.read_until('sername', TELNET_TIMEOUT)
+remote_conn.read_very_eager()
+print remote_conn
+# remote_conn.write(<command> + '\n')
+remote_conn.close()
+
+if __name__ == '__main__':
+    main()
