5ef284b413aa0cfa899f64dc8318a94d07ca6f27
Victor Palacio <vpalacio@ise.com>
2016-04-20 15:00:43 -0400

    Initial commit for Week 2

---
+++ excercises/Week 2/my_script.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+'''
+Python for Network Engineers: P4NE - Bring it!
+
+Excercise 1C
+'''
+from my_func import my_func
+
+__author__ = "Victor Palacio"
+__license__ = "GPL"
+__version__ = "1.0"
+__email__ = "Github - @vpalacio"
+
+my_func()
---
+++ excercises/Week 2/random_dir/my_func.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+'''
+Python for Network Engineers: P4NE - Bring it!
+
+Excercise 1C
+'''
+
+__author__ = "Victor Palacio"
+__license__ = "GPL"
+__version__ = "1.0"
+__email__ = "Github - @vpalacio"
+
+
+def my_func():
+    print "Hello"
