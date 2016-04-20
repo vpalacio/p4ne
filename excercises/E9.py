20aa2a6f997c7db9578dabe508905de0507c0c03
Victor Palacio <vpalacio@ise.com>
2016-04-20 13:52:07 -0400

    E9 excercise added

---
+++ excercises/E9.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+'''
+Python for Network Engineers: P4NE - Bring it!
+
+Excercise 9
+'''
+
+import yaml
+import json
+
+from pprint import pprint
+
+from ciscoconfparse import CiscoConfParse
+
+__author__ = "Victor Palacio"
+__license__ = "GPL"
+__version__ = "1.0"
+__email__ = "Github - @vpalacio"
+
+
+def main():
+
+    cisco_cfg = CiscoConfParse("cisco_ipsec.txt")
+
+    crypto_g2 = cisco_cfg.find_objects_w_child(parentspec=r"^crypto map CRYPTO", 
+                                               childspec=r"group2")
+
+    for parent in crypto_g2:
+      print parent.text
+      for child in parent.children:
+         print child.text
+
+if __name__ == '__main__':
+    main()
