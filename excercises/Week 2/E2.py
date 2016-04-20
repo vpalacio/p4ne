87ddfde1c1c3ff32bfc8207a347b70f3f9cb515e
Victor Palacio <vpalacio@ise.com>
2016-04-20 16:48:07 -0400

    tweak

--- excercises/Week 2/E2.py
+++ excercises/Week 2/E2.py
@@ -19,9 +19,17 @@ TELNET_TIMEOUT = 6
 def main():
 
 	ip_addr = '50.76.53.27'
+	username = 'pyclass'
+	password = '88newclass'
+
 	remote_conn = telnetlib.Telnet(ip_addr, TELNET_PORT, TELNET_TIMEOUT)
 	output = remote_conn.read_until("sername:", TELNET_TIMEOUT)
-	print output
+	remote_conn = remote_conn.write( username + '\n')
+    output = remote_conn.read_until("assword:", TELNET_TIMEOUT)
+    remote_conn = remote_conn.write( password + '\n')
+    output = remote_conn.read_very_eager()
+    print output
+
 	remote_conn.close()
 
 if __name__ == '__main__':
